b6c56a48e463978bd1abda9729afedbc854e3a36
seojanis <jennyjanjira9@gmail.com>
2025-06-17 07:13:16 +0000

    addwhatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,14 @@
+name = "jennie"
+last_name = "jenjira"
+
+s = f"Hey, what's your first name? : {name}"
+
+print(s)
+
+o = f"and your last name? : {last_name}"
+
+print(o)
+
+y = f"Well, pleasd to meet you, {name} {last_name}"
+
+print(y)
