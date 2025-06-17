c1014a521670e5d72e5dcc319636092d99099832
seojanis <jennyjanjira9@gmail.com>
2025-06-17 08:28:38 +0000

    addisneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+number = float(input("0:"))
+
+if number > 0:
+    print("This number is positive.")
+elif number < 0:
+    print("This number is negative.")
+else:
+    print("The number is zero.)")
